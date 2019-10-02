446ebcf44f5936272df1871ee2a3bb551a379762
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-10-02 09:26:04 +0200

    updatering
    Updatering för värde

--- Banken.py
+++ Banken.py
@@ -1,3 +1,7 @@
+import pickle
+import os 
+
+
 pin = 1234
 
 print("Hej, kära krister vänligen skriv ditt pinkod")
@@ -7,18 +11,30 @@ if pin != userPin:
     exit()
 
 menu = 0
+balance = 0.0
 # insättning
 # utttag
 # avslutning
 while menu != 3:
+    print("Ditt saldo är: ", balance)
+    menu = int(input("Skriv ditt val[1 insättning, 2 utaggning, 3 avsluta]: "))
     if menu == 1:
         print("insättning..")
         print( "\v")
-        a = float(input("sätt in ditt pengar"))
-        print(a)
-        
+        balance = balance + float(input("sätt in dina pengar: "))
+
     elif menu == 2:
-        print("insättning")
+        print("utaggning")
+        print( "\v")
+        if balance > input(float(-1)):
+            print("Invalid saldo")
+            
+        else:
+            balance = balance - float(input("sätt in dina pengar: "))
+        
+        
     elif menu == 3:
-        print("fel eller avsluta")
+        print("Avsluta")
         exit()
+    else:
+        print("Fel")
